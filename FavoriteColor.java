44d922e190a9b3ba98d916762593dabead04f4cc
Byron <byronblocker@gmail.com>
2017-04-24 11:55:59 -0700

    functionality to ask wheat color you like

--- FavoriteColor.java
+++ FavoriteColor.java
@@ -1,5 +1,5 @@
 public class FavoriteColor {
-  public static void main(String[] args); {
-    
+  public static void main(String[] args) {
+    System.out.println("What is your favorite color?");
   }
 }
