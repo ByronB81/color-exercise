b1060afff6d97989bc32d9202113d2e9ad6f0061
Byron <byronblocker@gmail.com>
2017-04-24 12:01:23 -0700

    add in response

--- FavoriteColor.java
+++ FavoriteColor.java
@@ -2,6 +2,10 @@ import java.io.Console;
 
 public class FavoriteColor {
   public static void main(String[] args) {
+    Console myConsole = System.console();
+
     System.out.println("What is your favorite color?");
+    String favoriteColor = myConsole.readline();
+    System.out.println("Your favorite color is... " + favoriteColor + "!");
   }
 }
